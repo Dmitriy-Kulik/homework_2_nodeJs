d6eb770eb47dd07369764e53c5c1c02854251f0f
Dmitriy-Kulik <0663642996d@gmail.com>
2018-02-18 18:35:04 +0200

    Redone on middleware functions

--- routes/myapp.js
+++ routes/myapp.js
@@ -1,17 +1,24 @@
 var express = require('express');
 var app = express.Router();
 
-app.get('/', function(req, res, next) {
-    res.render('hello', { title: 'Hello' });
-});
+app.get('/:id', function(req, res, next) {
+    if(req.params.id == 'console'){
+        res.render('console', {title: 'Look in console!'});
+        console.log("Hello console!");
+    }
+    else next();
 
-app.get('/hi', function(req, res, next) {
-    res.end('Hi world!');
-});
+}, function (req, res, next) {
+    if(req.params.id == 'hello'){
+        res.render('hello', { title: 'Hello' });
+    }
+    else next();
 
-app.get('/console', function(req, res, next) {
-    res.render('console', {title: 'Look in console!'});
-    console.log("Hello console!");
+}, function (req, res, next) {
+    if(req.params.id == 'hi') {
+        res.send('Hi world!');
+    }
+    else res.end('Enter in url console, hello or hi!');
 });
 
 module.exports = app;
